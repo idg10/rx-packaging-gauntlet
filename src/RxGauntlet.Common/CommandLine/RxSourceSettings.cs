f71f440dce4f2da970a618f49f7f63fa76d8cb98
Ian Griffiths <Ian.Griffiths@endjin.com>
2025-06-26 08:36:41 +0100

    Spectre.Console.Cli WIP

---
+++ src/RxGauntlet.Common/CommandLine/RxSourceSettings.cs
@@ -0,0 +1,16 @@
+﻿using Spectre.Console.Cli;
+
+using System.ComponentModel;
+
+namespace RxGauntlet.CommandLine;
+
+public class RxSourceSettings : CommandSettings
+{
+    [Description("The URL of an additional NuGet package source, or the file path of a local package store. (The public NuGet feed will remain available.)")]
+    [CommandOption("-ps|--package-source")]
+    public string? PackageSource { get; init; }
+
+    [Description("Package IDs of NuGet packages to add")]
+    [CommandOption("-pids|--package-ids")]
+    public required string[] PackageIds { get; init; }
+}
